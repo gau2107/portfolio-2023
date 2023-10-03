d0c9eccdc01cde7ebbff00a831c1dc7aa197764b
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-10-03 09:55:25 +0530

    add logo in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,12 @@
-
+import Image from "next/image";
 import styles from "./header.module.scss";
+import Logo from "../assets/logo.png";
 export default function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoBox}>
+        <Image src={Logo} alt="Logo" width={300} height={200}/>
+      </div>
+    </header>
+  );
 }
